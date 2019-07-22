71f88bc5f2c78a36be932f3a75549083502c9f84
adeevafatmah <53138560+adeevafatmah@users.noreply.github.com>
2019-07-23 04:31:25 +0700

    TakeImageApp
    Aplikasi Camera Android

---
+++ TakeImageApp/app/src/main/java/com/example/takeimageapp/SinglePhoto.java
@@ -0,0 +1,25 @@
+package com.example.takeimageapp;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SinglePhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_photo);
+
+        String imgPath = this.getIntent().getExtras().getString("posisi");
+        ImageView iv = (ImageView) findViewById(R.id.singleimage);
+        BitmapFactory.Options myBitmap = new BitmapFactory.Options();
+        myBitmap.inJustDecodeBounds= false;
+        myBitmap.inSampleSize=4;
+        myBitmap.inPurgeable=true;
+        Bitmap bitmap = BitmapFactory.decodeFile(imgPath, myBitmap);
+        iv.setImageBitmap(bitmap);
+    }
+}
